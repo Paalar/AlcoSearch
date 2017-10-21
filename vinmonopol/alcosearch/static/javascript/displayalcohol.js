af102a16fbe4bf4a3f4a49ba3ce6cfb0fce9ac1c
Pål Larsen <paaledward@gmail.com>
2017-10-21 04:53:44 +0200

    Added column sort functionality

--- vinmonopol/alcosearch/static/javascript/displayalcohol.js
+++ vinmonopol/alcosearch/static/javascript/displayalcohol.js
@@ -1,26 +1,12 @@
 $(document).ready(function() {
 
-  $(window).scroll(function(e){
-    var $el = $('#buttonRow');
-    var isPositionFixed = ($el.css('position') == 'fixed');
-    if ($(this).scrollTop() > 200 && !isPositionFixed){
-      $('#buttonRow').css({'position': 'fixed', 'top': '0px'});
-    }
-    if ($(this).scrollTop() < 200 && isPositionFixed)
-    {
-      $('#buttonRow').css({'position': 'static', 'top': '0px'});
-    }
+  $("#infoTable").DataTable( {
+    paging: false,
+    searching: false,
+    info: false,
+    "order": [[ 2, "asc" ]]
+
   });
 
-$("button").click(function(){
-  $("div.alkoholtype").hide();
-  var classN = this.className;
-  var str = classN.split(" ");
-  //$("div."+str).toggle();
-  if ($("div."+str).is(":visible")) {
-    $("div."+str).hide();
-  } else {
-    $("div."+str).show();
-  }
-  });
+
 });
