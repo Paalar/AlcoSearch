9213a6798f8ef035a567d70743af5c44b731878c
Pål Larsen <paaledward@gmail.com>
2017-10-24 11:02:12 +0200

    Made beer table scale properly

--- vinmonopol/vinmonopol/static/javascript/displayalcohol.js
+++ vinmonopol/vinmonopol/static/javascript/displayalcohol.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
 
+
+
   $("#infoTable").DataTable( {
+    paging: false,
+    searching: false,
+    info: false,
+    "order" : [[2, "asc"]]
+  });
+
+  $("#olInfo").DataTable( {
     paging: false,
     searching: false,
     info: false,
